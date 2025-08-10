28083e749d2ed71de6b83c4acbb522c2331d29a3
ZaraHabaeva <ZaraKhabaeva52@gmail.com>
2025-08-10 14:49:40 +0300

    1

--- src/components/Vc.vue
+++ src/components/Vc.vue
@@ -1,3 +1,31 @@
 <template>
-    <button>vc</button>
-  </template>
+  <div>
+    <div class="game-modal">
+      <div class="content">
+        <img src="images/lost.gif" alt="gif">
+        <h6></h6>
+        <p><b></b></p>
+        <button class="play-again">Play again</button>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="hangman-box">
+        <img src="images/hangman-0.svg" alt="hangman">
+        <h6>Hangman Game</h6>
+      </div>
+      <div class="game-box">
+        <ul class="word-display"></ul>
+        <h6 class="hint-text">
+          Hint:
+          <b>.</b>
+        </h6>
+        <h6 class="guesses-text">
+          Incorrect guesses:
+          <b>0/6</b>
+        </h6>
+        <div class="keyboard"></div>
+      </div>
+    </div>
+  </div>
+</template>
