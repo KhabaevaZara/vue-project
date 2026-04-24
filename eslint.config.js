7bbc55fb5f3053e7157ca29023e0f6538fcf1cb3
ZaraKhabaeva 💻🐈‍⬛️ <ZaraKhabaeva52@gmail.com>
2026-04-24 17:23:01 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,8 @@ import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
+
+
 export default defineConfig([
   {
     name: 'app/files-to-lint',
